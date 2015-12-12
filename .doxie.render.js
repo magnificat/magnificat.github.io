7312402ac77c2028f97cdc6dda177637c857acd5
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-12-12 12:35:31 +0100

    Even better styling

--- .doxie.render.js
+++ .doxie.render.js
@@ -1,5 +1,5 @@
 const slugify = require('./build/slugify');
 
 module.exports = (doc) => (
-  `  <a href="${slugify(doc.data)}">${doc.data}</a>\n`
+  `<a href="${slugify(doc.data)}">${doc.data}</a>\n\n`
 );
